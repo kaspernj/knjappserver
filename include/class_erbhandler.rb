b147e2c64829a04dbeedde0cfcc0d6beb037ed3c
Kasper Johansen <k@spernj.org>
2011-05-23 16:17:51 +0200

    Bugfix.

--- include/class_erbhandler.rb
+++ include/class_erbhandler.rb
@@ -1,38 +1,47 @@
 require "#{$knjappserver_config["knjrbfw"]}knj/erb/include.rb"
 
 class Knjappserver::ERBHandler
+	def initialize
+		@connected = false
+	end
+	
 	def erb_handler(data)
 		#Hack the Knj::Thread to accept data - this is how get, post and etc. are set.
 		Thread.current.data[:knjappserver] = data
 		
 		eruby = data[:httpsession].eruby
-		eruby.connect("error") do |e|
-			if _kas.config[:error_report_emails]
-				_kas.config[:error_report_emails].each do |email|
-					html = "An error occurred." + "<br /><br />"
-					html += "<b>#{e.class.name.html}: #{e.message.html}</b><br /><br />"
-					
-					#Lets hide all the stuff in what is not the users files to make it easier to debug.
-					bt = e.backtrace
-					to = bt.length - 9
-					bt = bt[0..to]
-					
-					bt.each do |line|
-						html += line.html + "<br />"
+		
+		if !@connected
+			eruby.connect("error") do |e|
+				if _kas.config[:error_report_emails]
+					_kas.config[:error_report_emails].each do |email|
+						html = "An error occurred." + "<br /><br />"
+						html += "<b>#{e.class.name.html}: #{e.message.html}</b><br /><br />"
+						
+						#Lets hide all the stuff in what is not the users files to make it easier to debug.
+						bt = e.backtrace
+						to = bt.length - 9
+						bt = bt[0..to]
+						
+						bt.each do |line|
+							html += line.html + "<br />"
+						end
+						
+						html += "<br />Post:<br /><pre>#{Knj::Php.print_r(_post, true)}</pre>"
+						html += "<br />Get:<br /><pre>#{Knj::Php.print_r(_get, true)}</pre>"
+						html += "<br />Server:<br /><pre>#{Knj::Php.print_r(_server, true).html}</pre>"
+						
+						mail = Knj::Mailobj.new(_kas.config[:smtp_args])
+						mail.to = email
+						mail.subject = sprintf("Error @ %s", _kas.config[:title])
+						mail.html = html
+						mail.from = _kas.config[:error_report_from]
+						mail.send
 					end
-					
-					html += "<br />Post:<br /><pre>#{Knj::Php.print_r(_post, true)}</pre>"
-					html += "<br />Get:<br /><pre>#{Knj::Php.print_r(_get, true)}</pre>"
-					html += "<br />Server:<br /><pre>#{Knj::Php.print_r(_server, true).html}</pre>"
-					
-					mail = Knj::Mailobj.new(_kas.config[:smtp_args])
-					mail.to = email
-					mail.subject = sprintf("Error @ %s", _kas.config[:title])
-					mail.html = html
-					mail.from = _kas.config[:error_report_from]
-					mail.send
 				end
 			end
+			
+			@connected = true
 		end
 		
 		cont = eruby.load_return(data[:filepath], {
