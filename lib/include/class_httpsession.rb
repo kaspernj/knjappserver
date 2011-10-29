aad50226139aeada298bf2ba0ec8c719e5aca4e9
Kasper Johansen <k@spernj.org>
2011-10-29 17:45:25 +0200

    Cookie optimizations.

--- lib/include/class_httpsession.rb
+++ lib/include/class_httpsession.rb
@@ -169,17 +169,14 @@ class Knjappserver::Httpsession
     end
     
     @session_id = nil
-    @session_id = "bot"  if @browser["browser"] == "bot"
-    @session_id = cookie["KnjappserverSession"] if cookie["KnjappserverSession"].to_s.length > 0
     
-    if !@session_id
+    if @browser["browser"] == "bot"
+      @session_id = "bot"
+    elsif cookie["KnjappserverSession"].to_s.length > 0
+      @session_id = cookie["KnjappserverSession"] 
+    else
       @session_id = @kas.session_generate_id(:meta => meta)
-      @resp.cookie(
-        "name" => "KnjappserverSession",
-        "value" => @session_id,
-        "path" => "/",
-        "expires" => Time.now + 32140800 #add around 12 months
-      )
+      send_cookie = true
     end
     
     begin
@@ -188,6 +185,10 @@ class Knjappserver::Httpsession
       #User should not have the session he asked for because of invalid user-agent or invalid IP.
       @session_id = @kas.session_generate_id(:meta => meta)
       session = @kas.session_fromid(:idhash => @session_id, :ip => @ip, :meta => meta)
+      send_cookie = true
+    end
+    
+    if send_cookie
       @resp.cookie(
         "name" => "KnjappserverSession",
         "value" => @session_id,
