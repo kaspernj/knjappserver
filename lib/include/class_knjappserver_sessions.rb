934aa37ffd023c96aeedce2f7f2447192b4102c5
Kasper Johansen <k@spernj.org>
2012-05-05 16:18:30 +0200

    Fixed problems with hash-thread-safety

--- lib/include/class_knjappserver_sessions.rb
+++ lib/include/class_knjappserver_sessions.rb
@@ -1,6 +1,6 @@
 class Knjappserver
   def initialize_sessions
-    @sessions = {}
+    @sessions = Knj::Threadsafe::Synced_hash.new
   end
   
   #Returns or adds session based on idhash and meta-data.
